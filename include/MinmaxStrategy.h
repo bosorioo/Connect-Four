b471f96f33bd8ae844e8055889c7231850082bb1
bosorioo <osorio.bruno@hotmail.com>
2015-03-20 02:53:29 -0400

    Fixed errors and increased performance.
    Changed "Game" variables to a reference so the copy-constructor overhead
    is avoided, and changed the heuristic calculation to make the algorithm
    work better (still not as good as it should be).

--- include/MinmaxStrategy.h
+++ include/MinmaxStrategy.h
@@ -9,7 +9,8 @@ public:
 	MinMaxStrategy(unsigned deepness = 3);
 	unsigned getNextPlay(Game current);
 	std::string getName();
-	int minmax_procedure(Game node, unsigned * nextMove, unsigned deepness);
+	int minmax_procedure(Game& node, unsigned * nextMove, unsigned deepness);
+	int minmax_ab_procedure(Game& node, unsigned * nextMove, unsigned deepness, int alpha, int beta);
 	int heuristic(Game node);
 private:
 	unsigned max_deep;
--- src/MinmaxStrategy.cpp
+++ src/MinmaxStrategy.cpp
@@ -4,8 +4,8 @@
 #include <cstdio>
 #include <cstdlib>
 
-#define MAXVALUE(a, b) ((a) > (b) ? (a) : (b))
-#define MINVALUE(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 MinMaxStrategy::MinMaxStrategy(unsigned deepness)
 {
@@ -16,6 +16,7 @@ unsigned MinMaxStrategy::getNextPlay(Game current)
 {
 	unsigned nextMove;
 	minmax_procedure(current, &nextMove, 0);
+	//minmax_ab_procedure(current, &nextMove, 0, INT_MIN, INT_MAX);
 	return nextMove;
 }
 
@@ -26,61 +27,134 @@ std::string MinMaxStrategy::getName()
 	return std::string(buffer);
 }
 
-int MinMaxStrategy::minmax_procedure(Game node, unsigned* nextMove, unsigned deepness)
+int MinMaxStrategy::minmax_procedure(Game& node, unsigned* nextMove, unsigned deepness)
 {
 	if (node.hasWinner())
-		return node.getWinner() == player_id ? 1 : -1;
+		return (max_deep + 1 - deepness) * (node.getWinner() == player_id ? 1 : -1);
 
 	if (node.hasTied() || deepness >= max_deep)
 		return 0;
 
 	int currentPlayer = node.getPlayerTurn();
 	int value, bestValue;
-	Game child;
+	unsigned i;
 
 	if (currentPlayer == player_id) // Maximize
 	{
 		bestValue = INT_MIN;
-		for (unsigned i = 0; i < node.getBoardWidth(); i++)
+
+		for (i = 0; i < node.getBoardWidth(); i++)
 		{
 			if (node.canPlay(currentPlayer, i))
 			{
-				child = node;
-				child.play(i);
-				value = minmax_procedure(child, 0, deepness + 1);
+				node.play(i);
+				value = minmax_procedure(node, 0, deepness + 1);
 
-
-				if (value > bestValue || child.getWinner() == currentPlayer ||
-					(value == bestValue && rand() % 2))
+				if (value > bestValue || node.getWinner() == currentPlayer ||
+					(bestValue == value && rand() % 2))
 				{
 					bestValue = value;
 					if (nextMove) *nextMove = i;
 				}
+				node.unplay(i);
 			}
 		}
 	}
 	else // Minimize
 	{
 		bestValue = INT_MAX;
-		for (unsigned i = 0; i < node.getBoardWidth(); i++)
+		for (i = 0; i < node.getBoardWidth(); i++)
 		{
 			if (node.canPlay(currentPlayer, i))
 			{
-				child = node;
-				child.play(i);
-				value = minmax_procedure(child, 0, deepness + 1);
+				node.play(i);
+				value = minmax_procedure(node, 0, deepness + 1);
 
-				if (value < bestValue || child.getWinner() == currentPlayer ||
-					(value == bestValue && rand() % 2))
+				if (value < bestValue || node.getWinner() == currentPlayer ||
+					(bestValue == value && rand() % 2))
 				{
 					bestValue = value;
 					if (nextMove) *nextMove = i;
 				}
+
+				node.unplay(i);
 			}
 		}
 	}
 
-	return bestValue;
+	return bestValue * (max_deep + 1 - deepness);
+}
+
+int MinMaxStrategy::minmax_ab_procedure(Game& node, unsigned* nextMove, unsigned deepness, int alpha, int beta)
+{
+	if (node.hasWinner())
+		return (max_deep + 1 - deepness) * (node.getWinner() == player_id ? 1 : -1);
+
+	if (node.hasTied() || deepness >= max_deep)
+		return 0;
+
+	int currentPlayer = node.getPlayerTurn();
+	int value;
+	unsigned i;
+
+	if (currentPlayer == player_id) // Maximize
+	{
+		value = INT_MIN;
+
+		for (i = 0; i < node.getBoardWidth(); i++)
+		{
+			if (node.canPlay(currentPlayer, i))
+			{
+				node.play(i);
+				value = MAX(value, minmax_ab_procedure(node, 0, deepness + 1, alpha, beta));
+				alpha = MAX(alpha, value);
+
+				if (beta <= alpha)
+				{
+				    node.unplay(i);
+                    break;
+				}
+
+				if (value > value || node.getWinner() == currentPlayer ||
+					(value == value && rand() % 2))
+				{
+					value = value;
+					if (nextMove) *nextMove = i;
+				}
+				node.unplay(i);
+			}
+		}
+	}
+	else // Minimize
+	{
+		value = INT_MAX;
+		for (i = 0; i < node.getBoardWidth(); i++)
+		{
+			if (node.canPlay(currentPlayer, i))
+			{
+				node.play(i);
+				value = MIN(value, minmax_ab_procedure(node, 0, deepness + 1, alpha, beta));
+				beta = MIN(beta, value);
+
+				if (beta <= alpha)
+				{
+				    node.unplay(i);
+                    break;
+				}
+
+				if (value < value || node.getWinner() == currentPlayer ||
+					(value == value && rand() % 2))
+				{
+					value = value;
+					if (nextMove) *nextMove = i;
+				}
+
+				node.unplay(i);
+			}
+		}
+	}
+
+	return value * (max_deep + 1 - deepness);
 }
 
 int MinMaxStrategy::heuristic(Game node)
